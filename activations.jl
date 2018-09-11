6d570b9e980c31358dff25ad4d8fee57783eabd0
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-09-11 17:29:38 -0400

    make more generic

--- activations.jl
+++ activations.jl
@@ -2,31 +2,39 @@ function sigmoid(x::Array{Float64})::Array{Float64}
     return 1 ./ (1 .+ exp.(-x))
 end
 
-function sigmoid(x::Float64)::Float64
+function sigmoid{T<:Number}(x::T)::T
     return 1 / (1 + exp(-x))
 end
 
-function hyperbolic_tan_sigmoid(x::Array{Float64})::Array{Float64}
+function hyperbolic_tan_sigmoid{T<:Number, N}(x::Array{T, N})::Array{T, N}
     return (exp.(x) .- exp.(-x)) ./ (exp.(x) + exp.(-x))
 end
 
-function hyperbolic_tan_sigmoid(x::Float64)::Float64
+function hyperbolic_tan_sigmoid{T<:Number}(x::T)::T
     return (exp(x) - exp(-x)) / (exp(x) + exp(-x))
 end
 
-function positive_linear!(x::Array{Float64})
+function positive_linear!{T<:Number, N}(x::Array{T, N})
     indx = x .< 0.
     x[indx] = [0. for i in 1:sum(Int64.(indx))]
     return 
 end
 
-function positive_linear(x::Array{Float64})::Array{Float64}
+function positive_linear{T<:Number, N}(x::Array{T, N})::Array{T, N}
     output = copy(x)
     indx = output .< 0.
     output[indx] = [0. for i in 1:sum(Int64.(indx))]
     return output
 end
 
-function positive_linear(x::Float64)::Float64
+function positive_linear{T<:Number}(x::T)::T
     return max(x, 0.)
+end
+
+function link{T<:Number, N}(x::Array{T, N})::Array{T, N}
+    return x
+end
+
+function link{T<:Number}(x::T)::T
+    return x
 end
