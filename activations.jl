6b8bf689a3c62b8c18a3ab76935b6e35039fbc0d
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-09-11 17:12:03 -0400

    le derp a derp

--- activations.jl
+++ activations.jl
@@ -26,3 +26,7 @@ function positive_linear(x::Array{Float64})::Array{Float64}
     output[indx] = [0. for i in 1:sum(Int64.(indx))]
     return output
 end
+
+function positive_linear(x::Float64)::Float64
+    return max(x, 0.)
+end
