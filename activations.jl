b51054f17fdbe85b4ebfa820b04381fd6aa1ae49
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-09-11 16:23:25 -0400

    Update activations.jl

--- activations.jl
+++ activations.jl
@@ -20,7 +20,7 @@ function positive_linear!(x::Array{Float64})
     return 
 end
 
-function positive_linear(x::Array{Float64})
+function positive_linear(x::Array{Float64})::Array{Float64}
     output = copy(x)
     indx = output .< 0.
     output[indx] = [0. for i in 1:sum(Int64.(indx))]
