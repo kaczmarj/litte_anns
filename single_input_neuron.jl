3caf11434050f33ff4e843a5b75255816140fa50
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-09-11 17:13:35 -0400

    Update single_input_neuron.jl

--- single_input_neuron.jl
+++ single_input_neuron.jl
@@ -3,4 +3,5 @@ include("Act.jl");
 input = rand();
 weight = 0.5;
 bias = 1.;
-output_relu = Act.positive_linear(input*weight + bias);
+output_relu = Act.positive_linear(input * weight + bias);
+output_sig = Act.sigmoid(input * weight + bias);
