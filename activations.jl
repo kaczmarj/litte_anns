cd807945e3162ce53640700ace4d33845e83c36b
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-09-11 16:16:52 -0400

    Update activations.jl

--- activations.jl
+++ activations.jl
@@ -14,9 +14,9 @@ function hyperbolic_tan_sigmoid(x::Float64)::Float64
     return (exp(x) - exp(-x)) / (exp(x) + exp(-x))
 end
 
-function positive_linear(x::Array{Float64})::Array{Float64}
+function positive_linear(x::Array{Float64})::nothing
     indx = x .< 0
     nz = sum(Int64.(indx))
     x[indx] = [0. for i in 1:nz]
-    return x
+    return nothing
 end
