0be2d052cadbdf119b309288f94f9dccdb9aead2
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-09-11 20:20:15 -0400

    baby steps

---
+++ single_layer_network.jl
@@ -0,0 +1,9 @@
+include("Act.jl");
+using Distributions
+
+N_DATA = 10;
+N_HIDDEN = 3;
+inputs = rand(Normal(0, 1), N_DATA);
+weight_matrix = rand(Float64, (N_HIDDEN, N_DATA));
+bias = ones(N_HIDDEN);
+output = Act.positive_linear(weight_matrix * inputs + bias);
