f691d23081e5e94165f2de04212ade98a2ed1abb
YSanchezAraujo <yoelsanchez006@gmail.com>
2018-09-13 11:02:16 -0400

    changing style to be 1.0 compatible

--- activations.jl
+++ activations.jl
@@ -1,26 +1,28 @@
-function sigmoid(x::Array{Float64})::Array{Float64}
+# various vanilla activation functions
+
+function sigmoid(x::Array{T})::Array{T} where {T<:Number}
     return 1 ./ (1 .+ exp.(-x))
 end
 
-function sigmoid{T<:Number}(x::T)::T
+function sigmoid(x::T)::T where {T<:Number}
     return 1 / (1 + exp(-x))
 end
 
-function hyperbolic_tan_sigmoid{T<:Number, N}(x::Array{T, N})::Array{T, N}
+function hyperbolic_tan_sigmoid(x::Array{T})::Array{T} where {T<:Number}
     return (exp.(x) .- exp.(-x)) ./ (exp.(x) + exp.(-x))
 end
 
-function hyperbolic_tan_sigmoid{T<:Number}(x::T)::T
+function hyperbolic_tan_sigmoid(x::T)::T where {T<:Number}
     return (exp(x) - exp(-x)) / (exp(x) + exp(-x))
 end
 
-function positive_linear!{T<:Number, N}(x::Array{T, N})
+function positive_linear!(x::Array{T}) where {T<:Number}
     indx = x .< 0.
     x[indx] = [0. for i in 1:sum(Int64.(indx))]
     return 
 end
 
-function positive_linear{T<:Number, N}(x::Array{T, N})::Array{T, N}
+function positive_linear(x::Array{T})::Array{T} where {T<:Number}
     output = copy(x)
     indx = output .< 0.
     output[indx] = [0. for i in 1:sum(Int64.(indx))]
@@ -29,14 +31,45 @@ end
 
 const INNER_ZERO_RETURN = Float64(0.);
 
-function positive_linear{T<:Number}(x::T)::Float64
+function positive_linear(x::T)::Float64 where {T<:Number}
     return max(x, INNER_ZERO_RETURN)
 end
 
-function link{T<:Number, N}(x::Array{T, N})::Array{T, N}
+function link(x::Array{T})::Array{T} where {T<:Number}
     return x
 end
 
-function link{T<:Number}(x::T)::T
+function link(x::T)::T where {T<:Number}
     return x
+end
+
+function sym_hardlim(x::Array{T})::Array{T} where {T<:Number}
+    output = ones(Float64, size(x, 1))
+    for (idx, val) in enumerate(x)
+	if val < 0
+	    output[idx] = -1.
+	end
+    end
+    return output
+end
+
+function sym_hardlim(x::T)::T where {T<:Number}
+    if x < 0
+	return -1.
+    end
+    return 1.
+end
+
+function satlin(x::Array{T})::Array{T} where {T<:Number}
+    output = ones(Float64, size(x, 1))
+    for (idx, val) in enumerate(x)
+	if val < 0
+	    output[idx] = 0.
+	else if 0 <= val <= 1
+	    output[idx] = val
+	else
+	    output[idx] = 1.
+	end
+    end
+    return output
 end
