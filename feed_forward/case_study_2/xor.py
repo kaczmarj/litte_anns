ac37ac2fd5c147fcdda84df7268ce5be3afe8335
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2019-01-19 15:20:46 -0500

    Update xor.py

--- feed_forward/case_study_2/xor.py
+++ feed_forward/case_study_2/xor.py
@@ -60,6 +60,11 @@ for epoch in np.arange(1000):
 
 # test the network on unseed data
 ntest = 400
-Xtest, ytest = return_ft(*make_xor_dataset(ntest))
-ypred = net(Xtest)
-print(t.sum(t.round(ypred.squeeze()) == ytest).item() / ntest)
+test_results  = np.zeros(1000)
+
+for idx in range(1000):
+    Xtest, ytest = return_ft(*make_xor_dataset(ntest))
+    ypred = net(Xtest)
+    test_results[idx] = t.sum(t.round(ypred.squeeze()) == ytest).item() / ntest
+
+print(test_results.mean())
